e5d98847ed24b56382271761ef211585660180c1
romankrru <rm07ru@gmail.com>
2018-09-29 22:19:34 +0300

    Connect to redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,22 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Hello from './components/Hello';
+import Hello from './containers/Hello';
+import {createStore} from 'redux';
+import {enthusiasm} from './reducers';
+import {StoreState} from './types';
+import {EnthusiasmAction} from './actions';
+import {Provider} from 'react-redux';
+
+const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={10} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
